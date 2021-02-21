d2898d8ea70793ca9969c28d98991cbb22095607
crc8109 <crc8109@pm.me>
2021-02-20 23:09:19 -0500

    First edits for client-facing app

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 # Monster Job Web Scraper
 [![Run on Repl.it](https://repl.it/badge/github/crc8109/job-scraper)](https://repl.it/github/crc8109/job-scraper)
 
-I used a tutorial from [Real Python](https://realpython.com/beautiful-soup-web-scraper-python/) and [Martin Breuss](https://github.com/martin-martin) to build a tool to help cut down on the noise in the job hunting process.
+A web-scrapping tool to help cut down on the noise in the job hunting process.
 
-
-
-Users pass along arguments to the script (lie a language e.g. Python and a location e.g. Chicago) so that users can get more tailored result. It's a CLI app but I'm hosting it on Replit to make it accesible to anyone. In the future, you can pass along another argument (called Role) so that you can look for more relevant positions (e.g front end, data scientist, etc).
+Pass along arguments to the script (e.g language, location, etc) to get more tailored result. It's a CLI app but I'm hosting it on Replit to make it accessible to anyone.
 
 ## What This Includes
 
@@ -29,4 +27,4 @@ pip install -r requirements.txt
 ```
 Run ```job_scraper.py``` to start scraping for jobs.
 
-If this doesn't work, it could be Monster updated their layout of their site, so my script will need updating (the woes of web scrapping). But feel free to reach out if you need anything.
+If this doesn't work, try changing your search criteria. Or, it could be Monster updated their layout of their site, so my script will need updating (the woes of web scrapping). But feel free to reach out if you need anything.
--- job_scraper.py
+++ job_scraper.py
@@ -1,55 +1,105 @@
 import requests
 from bs4 import BeautifulSoup
+from textwrap import dedent
 
-language = input("Language to search jobs for or leave blank and press ENTER: ")
-location = input("City OR state to search for jobs or leave blank and press ENTER: ").replace(' ', '-')
+def find_me_jobs():
 
-if language:
+    # language = input(dedent("""\
+    #     Enter a language to search jobs (e.g. Java or java) or leave blank to see
+    #     all developer jobs in your area and press ENTER: """)).replace(' ', '-')
+    #
+    # location = input(dedent("""
+    #     Enter City, State abbreviation (Chicago, IL)to search for jobs or leave
+    #     blank and press ENTER: """ )).replace(' ', '-').replace(',', '__2C')
 
-    #Language and location
-    if location:
-        URL = f'https://www.monster.com/jobs/search/?q={language}&where={location}&stpage=1&page=3'
+    language = "python"
+    location = "Framingham__2C-MA"
 
-    #Only language
+    if language:
+        #Language and location
+        if location:
+            URL = dedent(f"""\
+            https://www.monster.com/jobs/search/?q={language}&where={location}&st
+            page=1&page=3
+            """).replace("\n", "")
+
+        #Only language
+        else:
+            URL = dedent(f"""
+            https://www.monster.com/jobs/search/?q={language}&stpage=1&page=3"""
+            ).replace("\n", "")
+
+    #if only location
+    elif location:
+        URL = dedent(f"""\
+        https://www.monster.com/jobs/search/?q=Software-Developer&where={location}
+        &stpage=1&page=3""").replace("\n", "")
+
+    #if nothing
     else:
-        URL = f'https://www.monster.com/jobs/search/?q={language}&stpage=1&page=3'
+        URL = dedent(f"""https://www.monster.com/jobs/search/?q=Software-Developer""")
 
-#if only location
-elif location:
-    URL = f'https://www.monster.com/jobs/search/?where={location}&stpage=1&page=3'
-
-#if nothing
-else:
-    URL = f'https://www.monster.com/jobs/search/?q=Software-Developer'
-
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
-job_elems = results.find_all('section', class_='card-content')
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(id='ResultsContainer')
+    job_elems = results.find_all('section', class_='card-content')
 
 
-print("\n", end='')
-print("*" * 75, "\n")
-input("Search results loaded. \nPress ENTER to print results:")
+    print("\n", end='')
+
+    counter = 0
+
+    def count_n_load_jobs(jobs, counter):
+        # this will compile all viable jobs
 
 
-#Return results of job search
-print("Printing jobs:")
-# this will print every job posting as text
-for job_elem in job_elems:
+        for job_elem in job_elems:
+            company_elem = job_elem.find('div', class_='company')
+            counter += 1
 
-    title_elem = job_elem.find('h2', class_='title', string=lambda text: f'{language}' in text.lower())
-    company_elem = job_elem.find('div', class_='company')
-    location_elem = job_elem.find('div', class_='location')
-    link = job_elem.find('a')
+            if None in job_elem:
+                counter -= 1
+        print(counter)
+        input("test")
+            # title_elem = job_elem.find('h2', class_='title', string=lambda text: f'{language}' in text.lower())
+            # company_elem = job_elem.find('div', class_='company')
+            # location_elem = job_elem.find('div', class_='location')
+            # link = job_elem.find('a')
+            #
+            # # #monster has a Section html element that DOESN"T have info, so without this,
+            # # #it'll return an error with the print statements afterwards
+            # if None in (title_elem, company_elem, location_elem, link):
+            #     counter -= 1
+            #     continue
 
-    #monster has a Section html element that DOESN"T have info, so without this,
-    #it'll return an error with the print statements afterwards
-    if None in (title_elem, company_elem, location_elem, link):
-        continue
+        print(f"Search results loaded. {counter} found.")
+        input("Press ENTER to print results:")
+        print("*" * 75, "\n")
 
-    print(title_elem.text.strip())
-    print(company_elem.text.strip())
-    print(f"{location_elem.text.strip()}")
-    print(f"Apply here: {link['href']}\n")
-    print("-" * 75,'\n' )
+        #Return results of job search
+        print("Printing jobs:\n")
+        # this will print every job posting as text
+        for job_elem in job_elems:
+
+            title_elem = job_elem.find('h2', class_='title', string=lambda text: f'{language}' in text.lower())
+            company_elem = job_elem.find('div', class_='company')
+            location_elem = job_elem.find('div', class_='location')
+            age = job_elem.find('time')
+            link = job_elem.find('a')
+
+            #monster has a Section html element that DOESN"T have info, so without this,
+            #it'll return an error with the print statements afterwards
+            if None in (title_elem, company_elem, location_elem, link):
+                continue
+
+            print(title_elem.text.strip())
+            print(company_elem.text.strip())
+            print(f"{location_elem.text.strip()}")
+            print(f"Apply here: {link['href']}")
+            print(f"Age: {age.text.strip()}\n")
+            print("-" * 75,'\n' )
+
+    count_n_load_jobs(job_elems, 0)
+    return
+
+find_me_jobs()
