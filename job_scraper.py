93fbd3e4431c491fd87a3d4f32ea9f8cd157ad81
crc8109 <crc8109@pm.me>
2021-02-20 23:38:19 -0500

    Fixed display issue with job count and cleaned up app to be MVP

--- job_scraper.py
+++ job_scraper.py
@@ -3,77 +3,54 @@ from bs4 import BeautifulSoup
 from textwrap import dedent
 
 def find_me_jobs():
+    language = input(dedent("""\
+        Enter a language to search jobs (e.g. Java or java) or leave blank to see
+        all developer jobs in your area and press ENTER: """)).replace(' ', '-')
 
-    # language = input(dedent("""\
-    #     Enter a language to search jobs (e.g. Java or java) or leave blank to see
-    #     all developer jobs in your area and press ENTER: """)).replace(' ', '-')
-    #
-    # location = input(dedent("""
-    #     Enter City, State abbreviation (Chicago, IL)to search for jobs or leave
-    #     blank and press ENTER: """ )).replace(' ', '-').replace(',', '__2C')
+    #formatting is based on how Monster uses location
+    location = input(dedent("""
+        Enter either a City, State, or both (like this: Chicago, IL) to search
+        for jobs or leave blank and press ENTER: """ )).replace(' ', '-').replace(',', '__2C')
 
-    language = "python"
-    location = "Framingham__2C-MA"
+    # language = "python"
+    # location = "Chicago__2C-IL"
 
     if language:
         #Language and location
         if location:
             URL = dedent(f"""\
-            https://www.monster.com/jobs/search/?q={language}&where={location}&st
+            https://www.monster.com/jobs/search/?q={language}&where={location}&rad=50&st
             page=1&page=3
             """).replace("\n", "")
 
         #Only language
         else:
             URL = dedent(f"""
-            https://www.monster.com/jobs/search/?q={language}&stpage=1&page=3"""
+            https://www.monster.com/jobs/search/?q={language}&rad=50&stpage=1&page=3"""
             ).replace("\n", "")
 
     #if only location
     elif location:
         URL = dedent(f"""\
         https://www.monster.com/jobs/search/?q=Software-Developer&where={location}
-        &stpage=1&page=3""").replace("\n", "")
+        &rad=50&stpage=1&page=3""").replace("\n", "")
 
     #if nothing
     else:
-        URL = dedent(f"""https://www.monster.com/jobs/search/?q=Software-Developer""")
+        URL = dedent(f"""https://www.monster.com/jobs/search/?q=Software-Developer&rad=50""")
 
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
     results = soup.find(id='ResultsContainer')
     job_elems = results.find_all('section', class_='card-content')
-
-
+    valid_jobs = results.find_all('h2',
+                          string=lambda text: f'{language}' in text.lower())
     print("\n", end='')
+    print(f"Search results loaded. {len(valid_jobs)} found.")
+    input("Press ENTER to print results:")
 
-    counter = 0
-
-    def count_n_load_jobs(jobs, counter):
+    def print_job_details(jobs):
         # this will compile all viable jobs
-
-
-        for job_elem in job_elems:
-            company_elem = job_elem.find('div', class_='company')
-            counter += 1
-
-            if None in job_elem:
-                counter -= 1
-        print(counter)
-        input("test")
-            # title_elem = job_elem.find('h2', class_='title', string=lambda text: f'{language}' in text.lower())
-            # company_elem = job_elem.find('div', class_='company')
-            # location_elem = job_elem.find('div', class_='location')
-            # link = job_elem.find('a')
-            #
-            # # #monster has a Section html element that DOESN"T have info, so without this,
-            # # #it'll return an error with the print statements afterwards
-            # if None in (title_elem, company_elem, location_elem, link):
-            #     counter -= 1
-            #     continue
-
-        print(f"Search results loaded. {counter} found.")
-        input("Press ENTER to print results:")
         print("*" * 75, "\n")
 
         #Return results of job search
@@ -95,11 +72,11 @@ def find_me_jobs():
             print(title_elem.text.strip())
             print(company_elem.text.strip())
             print(f"{location_elem.text.strip()}")
-            print(f"Apply here: {link['href']}")
-            print(f"Age: {age.text.strip()}\n")
+            print(f"Age: {age.text.strip()}")
+            print(f"Apply here: {link['href']}\n")
             print("-" * 75,'\n' )
 
-    count_n_load_jobs(job_elems, 0)
+    print_job_details(valid_jobs)
     return
 
 find_me_jobs()
